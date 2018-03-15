3a090d5e348696ce804bc4a4f28cb00561d85fac
ink-su <6701545+ink-su@users.noreply.github.com>
2018-03-15 01:14:24 -0400

    Added some ratios to conversion utility

--- src/lib/services/dataConversionUtility.js
+++ src/lib/services/dataConversionUtility.js
@@ -7,16 +7,32 @@ const levels = nconf.get('renderd:maxzoom');
 const DEG_TO_RAD = Math.pi / 180;
 const RAD_TO_DEG = 180 / Math.pi;
 
-function minmax(a, b, c) {
-  return Math.min(Math.max(a, b), c);
+// These are ratio values for converting something?
+const Bc = [];
+const Cc = [];
+const zc = [];
+const Ac = [];
+let c = 256; // Is this tile size?
+for (let i = 0; i < levels + 1; i += 1) {
+  const e = c / 2; // Is this midpoint x,y of the tile?
+  Bc.push(c / 360);
+  Cc.push(c / (2 * Math.Pi));
+  zc.push([e, e]);
+  Ac.push(c);
+  c *= 2;
+}
+
+
+function minMax(x, y, z) {
+  return Math.min(Math.max(x, y), z);
 }
 
 module.exports = {
-  longLatToPixel: (long, lat, zoom) => {
+  longLatToPixel: (longLat, level) => {
 
   },
 
-  pixedToLongLat: (x, y) => {
+  pixedToLongLat: (xy, level) => {
 
   },
 };
